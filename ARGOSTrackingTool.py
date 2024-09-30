d6fb5987d17fe529cc0040d598e2c30e57b7e079
yk5012 <yujin.kim5012@gmail.com>
2024-09-30 11:35:34 -0400

    Initial commit of script document

---
+++ ARGOSTrackingTool.py
@@ -0,0 +1,11 @@
+#-------------------------------------------------------------
+# ARGOSTrackingTool.py
+# this is called front matter(similar to README)
+#
+# Description: Reads in an ARGOS tracking data file and allows
+#   the user to view the location of the turtle for a specified
+#   date entered via user input.
+#
+# Author: John Fay (john.fay@duke.edu)
+# Date:   Fall 2024
+#--------------------------------------------------------------
